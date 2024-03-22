967e3cda1bb04de56af48af7a69c5b432e1f1b94
5Marq <epgawin@yahoo.pl>
2024-03-22 16:47:13 +0100

    Project fix, UI, no product choice in constructor

--- tutorial2/RefrigiratedContainer.cs
+++ tutorial2/RefrigiratedContainer.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Diagnostics;
+
 namespace tutorial2
 {
     public class RefrigiratedContainer : Container, IHazardNotifier
@@ -5,37 +8,78 @@ namespace tutorial2
         private string cargoType = "";
         private double temperature = 0;
         private Product product;
+
         public RefrigiratedContainer(double weight, double height, double netWeight, double depth, char type,
-            double maxLoad, string cargoType) : base(weight, height, netWeight, depth, type, maxLoad)
+            double maxLoad, double temperature) : base(weight, height, netWeight, depth, type, maxLoad)
         {
-            this.cargoType = cargoType;
+            this.temperature = temperature;
         }
+
         public void notify()
         {
-            System.Console.WriteLine("Niebezpieczna sytuacja! Numer kontenera: "+getSerialNunber());
+            System.Console.WriteLine("Niebezpieczna sytuacja! Numer kontenera: " + getSerialNunber());
         }
-        
+
         protected override void unloadCargo()
         {
             setCargoWeight(0);
         }
-        
+
         protected override void loadCargo(double cargoWeight)
         {
-            Product product = new Product(cargoType, temperature); //todo let the user choose the cargo and then create the product
+            Product
+                product = new Product(cargoType,
+                    temperature); //todo let the user choose the cargo and then create the product
             //todo get rid of choosing product from the constructor
-            
-            if (cargoWeight > getMaxLoad())
+
+            Console.WriteLine("Choose cargo type:");
+            Console.WriteLine("1. Bananas");
+            Console.WriteLine("2. Chocolate");
+            Console.WriteLine("3. Fish");
+            Console.WriteLine("4. Meat");
+
+            string choice = Console.ReadLine();
+
+            switch (choice)
             {
-                notify();
+                case "1":
+                {
+                    product = new Product("Bananas", temperature);
+                    break;
+                }
+                case "2":
+                {
+                    product = new Product("Chocolate", temperature);
+                    break;
+                }
+                case "3":
+                {
+                    product = new Product("Fish", temperature);
+                    break;
+                }
+                case "4":
+                {
+                    product = new Product("Meat", temperature);
+                    break;
+                    
+                }
+                default:
+                {
+                    Console.WriteLine("Invalid choice!");
+                    break;
+                }
             }
-            else
-            {
-                setCargoWeight(cargoWeight);
+            
+
+
+            if (cargoWeight > getMaxLoad())
+                    {
+                        notify();
+                    }
+                    else
+                    {
+                        setCargoWeight(cargoWeight);
+                    }
             }
         }
-        
-        
-        
     }
-}
--- tutorial2/main.cs
+++ tutorial2/main.cs
@@ -1,10 +1,81 @@
-﻿namespace tutorial2
+﻿using System;
+using System.Diagnostics;
+
+namespace tutorial2
 {
     public class main
     {
         public static void Main(string[] args)
         {
+            
+            Console.WriteLine("===Welcome to the ship simulation program===");
+            Console.WriteLine("================= MENU =====================");
+            bool programState = true;
+
+            while (programState)
+            {
+                Console.WriteLine("1. Add a new container");
+                Console.WriteLine("2. Load cargo to the container");
+                Console.WriteLine("3. Add a new ship");
+                Console.WriteLine("4. Load cargo to the ship");
+                Console.WriteLine("5. Unload container from the ship");
+                Console.WriteLine("6. Unload cargo from the container");
+                Console.WriteLine("7. Show all containers");
+                Console.WriteLine("8. Show all ships");
+                Console.WriteLine("9. Exit program");
+
+                string choice = Console.ReadLine();
+                
+                switch(choice)
+                {
+                    case "1":
+                    {
+                        break;
+                    }
+                    case "2":
+                    {
+                        break;
+                    }
+                    case "3":
+                    {
+                        break;
+                    }
+                    case "4":
+                    {
+                        break;
+                    }
+                    case "5":
+                    {
+                        break;
+                    }
+                    case "6":
+                    {
+                        break;
+                    }
+                    case "7":
+                    {
+                        break;
+                    }
+                    case "8":
+                    {
+                        break;
+                    }
+                    case "9":
+                    {
+                        programState = false;
+                        break;
+                    }
+                    default:
+                    {
+                        Console.WriteLine("Invalid choice!");
+                        break;
+                    }
+                }
+
+            }
+            Console.WriteLine("Program terminated");
             //todo UI
+            //todo add ship class
         }
     }
 }
